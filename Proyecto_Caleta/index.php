c6850f25444bcef99c292c4eca5902bb95538b47
AleJhunior10 <alejhunior10@gmail.com>
2021-06-02 10:15:19 -0400

    Update

---
+++ Proyecto_Caleta/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./sistema/css/estilos.css">
+</head>
+
+<body>
+    <div class="fondo">
+        <div class="capa-gradiente"></div>
+
+        <div class="scroll-up-btn">
+            <i class="fas fa-angle-up"></i>
+        </div>
+        <nav class="navbar">
+            <div class="max-width">
+                <div class="logo"><a href="#">La<span>Caleta</span></a></div>
+                <ul class="menu">
+                    <li><a href="home.php" class="menu-btn">Sistema de Ventas</a></li>
+                </ul>
+                <div class="menu-btn">
+                    <i class="fas fa-bars"></i>
+                </div>
+            </div>
+        </nav>
+        <div class="centro">
+            <section class="home" id="home">
+
+                <div class="max-width">
+                    <div class="home-content">
+                        <div class="text-1">Bienvenido a</div>
+                        <div class="text-2">La Caleta</div>
+                        <div class="text-3">Bar Restaurante Cafeteria<span class="typing"></span></div>
+                        <a href="home.php">Iniciar</a>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+
+</body>
+
+</html>
